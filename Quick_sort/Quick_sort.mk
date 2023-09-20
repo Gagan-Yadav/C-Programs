a9cf99cd3cc4acf1205d3d86c99661728e5b36d6
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-20 20:48:16 +0530

    Update Quick_sort.mk

--- Quick_sort/Quick_sort.mk
+++ Quick_sort/Quick_sort.mk
@@ -7,7 +7,7 @@ WorkspaceConfiguration := $(ConfigurationName)
 WorkspacePath          :=C:/Users/ASUS/OneDrive/Documents/My_programmes
 ProjectPath            :=C:/Users/ASUS/OneDrive/Documents/My_programmes/Quick_sort
 IntermediateDirectory  :=../build-$(ConfigurationName)/Quick_sort
-OutDir                 :=../build-$(ConfigurationName)/Quick_sort
+OutDir          me)/Quick_sort
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
