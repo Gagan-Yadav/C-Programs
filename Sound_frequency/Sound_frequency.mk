0e745994b1347d01b8710c0531cedb91cc043362
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-28 12:58:23 +0530

    Update Sound_frequency.mk

--- Sound_frequency/Sound_frequency.mk
+++ Sound_frequency/Sound_frequency.mk
@@ -3,7 +3,7 @@
 ## any manual changes will be erased      
 ##
 ## Debug
-ProjectName            :=Sound_frequency
+ProjectName            :
 ConfigurationName      :=Debug
 WorkspaceConfiguration := $(ConfigurationName)
 WorkspacePath          :=C:/Users/ASUS/OneDrive/Documents/My_programmes
